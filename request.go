a1cc827d4f197a4358f766ed5c886191482932c4
sample <global-config@example.com>
2022-01-03 08:48:09 +0800

    fx

--- request.go
+++ request.go
@@ -1,9 +1,6 @@
 package nrpc
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/nats-io/nats.go"
 )
 
@@ -12,47 +9,47 @@ type Request[T any] struct {
 	Data *T
 }
 
-func NewRequest[T1 any, T2 any](conn *nats.Conn, topic string) *Requester[T1, T2] {
-	return &Requester[T1, T2]{
-		conn:              conn,
-		topic:             topic,
-		responseValidator: func(out *Response[T2]) error { return nil },
-	}
-}
+// func NewRequest[T1 any, T2 any](conn *nats.Conn, topic string) *Requester[T1, T2] {
+// 	return &Requester[T1, T2]{
+// 		conn:              conn,
+// 		topic:             topic,
+// 		responseValidator: func(out *Response[T2]) error { return nil },
+// 	}
+// }
 
-type Requester[T1 any, T2 any] struct {
-	conn              *nats.Conn
-	topic             string
-	responseValidator func(out *Response[T2]) error
-}
+// type Requester[T1 any, T2 any] struct {
+// 	conn              *nats.Conn
+// 	topic             string
+// 	responseValidator func(out *Response[T2]) error
+// }
 
-func (m *Requester[T1, T2]) Validator(fc func(out *Response[T2]) error) *Requester[T1, T2] {
-	m.responseValidator = fc
-	return m
-}
+// func (m *Requester[T1, T2]) Validator(fc func(out *Response[T2]) error) *Requester[T1, T2] {
+// 	m.responseValidator = fc
+// 	return m
+// }
 
-func (m *Requester[T1, T2]) Request(in *T1, timeout time.Duration) (*Response[T2], error) {
-	mr, err := encode(in)
-	if err != nil {
-		return nil, err
-	}
-	mr.Subject = fmt.Sprintf("%s%s", pre, m.topic)
-	res, err := m.conn.RequestMsg(mr, timeout)
-	if err != nil {
-		return nil, err
-	}
+// func (m *Requester[T1, T2]) Request(in *T1, timeout time.Duration) (*Response[T2], error) {
+// 	mr, err := encode(in)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	mr.Subject = fmt.Sprintf("%s%s", pre, m.topic)
+// 	res, err := m.conn.RequestMsg(mr, timeout)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	tmp, err := decode[T2](res)
-	if err != nil {
-		return nil, err
-	}
-	out := &Response[T2]{
-		Raw:  res,
-		Data: tmp,
-	}
-	if err := m.responseValidator(out); err != nil {
-		return nil, err
-	} else {
-		return out, nil
-	}
-}
+// 	tmp, err := decode[T2](res)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	out := &Response[T2]{
+// 		Raw:  res,
+// 		Data: tmp,
+// 	}
+// 	if err := m.responseValidator(out); err != nil {
+// 		return nil, err
+// 	} else {
+// 		return out, nil
+// 	}
+// }
--- service.go
+++ service.go
@@ -4,27 +4,31 @@ import (
 	"fmt"
 	"log"
 	"runtime"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
 
 const pre = "_NRPC."
 
-func New[T1 any, T2 any](conn *nats.Conn) *Servicer[T1, T2] {
+func New[T1 any, T2 any](conn *nats.Conn, topic string) *Servicer[T1, T2] {
 	return &Servicer[T1, T2]{
-		conn:             conn,
-		requestValidator: func(req *Request[T1]) error { return nil },
+		conn:  conn,
+		topic: topic,
 	}
 }
 
 type Response[T any] struct {
-	Raw  *nats.Msg
+	Raw *nats.Msg
+
 	Data *T
 }
 
 type Servicer[T1 any, T2 any] struct {
-	conn             *nats.Conn
-	requestValidator func(in *Request[T1]) error
+	conn              *nats.Conn
+	topic             string
+	requestValidator  func(in *Request[T1]) error
+	responseValidator func(out *Response[T2]) error
 }
 
 func (m *Servicer[T1, T2]) Handle(msg *nats.Msg, cb func(in *Request[T1]) (*T2, error)) (ret *nats.Msg, err error) {
@@ -48,9 +52,12 @@ func (m *Servicer[T1, T2]) Handle(msg *nats.Msg, cb func(in *Request[T1]) (*T2, 
 		Raw:  msg,
 		Data: tmp,
 	}
-	if err := m.requestValidator(req); err != nil {
-		return nil, err
-	} else if res, err := cb(req); err != nil {
+	if m.requestValidator != nil {
+		if err := m.requestValidator(req); err != nil {
+			return nil, err
+		}
+	}
+	if res, err := cb(req); err != nil {
 		return nil, err
 	} else {
 		return encode(res)
@@ -62,20 +69,62 @@ func (m *Servicer[T1, T2]) Validator(fc func(in *Request[T1]) error) *Servicer[T
 	return m
 }
 
-func (m *Servicer[T1, T2]) Queue(subj string, queue string, cb func(in *Request[T1]) (*T2, error)) (*nats.Subscription, error) {
-	return m.conn.QueueSubscribe(fmt.Sprintf("%s%s", pre, subj), queue, func(msg *nats.Msg) {
-		res, err := m.Handle(msg, cb)
-		if err != nil {
-			res = encodeError(err)
-		}
-		msg.RespondMsg(res)
-	})
+func (m *Servicer[T1, T2]) Validator2(fc func(out *Response[T2]) error) *Servicer[T1, T2] {
+	m.responseValidator = fc
+	return m
 }
 
-func (m *Servicer[T1, T2]) Sub(subj string, cb func(in *Request[T1]) (*T2, error)) (*nats.Subscription, error) {
-	return m.conn.Subscribe(fmt.Sprintf("%s%s", pre, subj), func(msg *nats.Msg) {
+func (m *Servicer[T1, T2]) Request(in *T1, timeout time.Duration) (*Response[T2], error) {
+	mr, err := encode(in)
+	if err != nil {
+		return nil, err
+	}
+	mr.Subject = fmt.Sprintf("%s%s", pre, m.topic)
+	res, err := m.conn.RequestMsg(mr, timeout)
+	if err != nil {
+		return nil, err
+	}
+	tmp, err := decode[T2](res)
+	if err != nil {
+		return nil, err
+	}
+	out := &Response[T2]{
+		Raw:  res,
+		Data: tmp,
+	}
+	if m.responseValidator != nil {
+		if err := m.responseValidator(out); err != nil {
+			return nil, err
+		}
+	}
+	return out, nil
+}
+
+func (m *Servicer[T1, T2]) Queue(cb func(in *Request[T1]) (*T2, error), queues ...string) ([]*nats.Subscription, error) {
+	rets := make([]*nats.Subscription, 0)
+	arr := queues
+	if len(arr) == 0 {
+		arr = append(arr, "worker")
+	}
+	for i := 0; i < len(arr); i++ {
+		res, err := m.conn.QueueSubscribe(fmt.Sprintf("%s%s", pre, m.topic), arr[i], func(msg *nats.Msg) {
+			res, err := m.Handle(msg, cb)
+			if err != nil {
+				res = encodeError(err)
+			}
+			msg.RespondMsg(res)
+		})
+		if err != nil {
+			return nil, err
+		}
+		rets = append(rets, res)
+	}
+	return rets, nil
+}
+
+func (m *Servicer[T1, T2]) Sub(cb func(in *Request[T1]) (*T2, error)) (*nats.Subscription, error) {
+	return m.conn.Subscribe(fmt.Sprintf("%s%s", pre, m.topic), func(msg *nats.Msg) {
 		if res, err := m.Handle(msg, cb); err != nil {
-			log.Println("handle", err)
 			msg.RespondMsg(encodeError(err))
 		} else {
 			msg.RespondMsg(res)
