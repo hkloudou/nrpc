7c89f8a95059369b6b0d374a5773e3349841a50e
sample <global-config@example.com>
2022-05-21 09:08:38 +0800

    auto commit face

--- face/face.go
+++ face/face.go
@@ -3,5 +3,5 @@ package face
 type Codec interface {
 	Check(val interface{}) bool
 	Marshal(val interface{}) ([]byte, error)
-	Unmarshal(val interface{}, data []byte) error
+	Unmarshal(data []byte, val interface{}) error
 }
